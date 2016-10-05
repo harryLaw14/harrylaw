a45b6afe774863872eb036acaa0b62c42d177bcd
harryLaw14 <17lawh76@millshighsch.org>
2016-10-05 11:08:05 -0700

    collab done

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -9,19 +9,19 @@
 public class Calculate {
 	
 	public static int square(int num) {
-		return(num*num);
+		return(num*num); //squares a number
 	}
 	
 	public static int cube(int num) {
-		return(num*num*num);
+		return(num*num*num); //cubes a number
 	}
 	
 	public static double average(double num1, double num2) {
-		return((num1 + num2)/2);
+		return((num1 + num2)/2); //takes the average of 2 numbers
 	}
 	
-	public static double average2(double num1, double num2, double num3) {
-		return((num1 + num2 + num3)/2);
+	public static double average(double num1, double num2, double num3) {
+		return((num1 + num2 + num3)/2); //takes the average of 3 numbers
 	}
 	
 	public static double toDegrees(double num) {
@@ -33,11 +33,11 @@ public class Calculate {
 	}
 	
 	public static double discriminant(double num1, double num2, double num3) {
-		return ((num2*num2)-4*(num1*num3));
+		return ((num2*num2)-4*(num1*num3));  
 	}
 	
 	public static int toImproperFrac(int A, int B, int C) {
-		return ((C*A)/C);
+		return ((C*A)/C); 
 	}
 		
 	public static double toMixedNum(double A, double B, double C) {
@@ -45,20 +45,21 @@ public class Calculate {
 	}
 	
 	public static int foil(int num1, int num2, int num3, int num4, int n) {
-		return (((num1*n)+num2)*((num3*n)-num4));
+		return (((num1*n)+num2)*((num3*n)-num4)); 
 	}
 	
 	public static boolean isDivisibleBy(int x, int y) {
-		
+		//returns true if the remainder is zero
+		//returns false if the remainder is greater than zero
 		if (x%y == 0) {
 			return true;
-		} else {
+		} else { 
 			return false;
 		}
 	}
 	
 	public static double absValue(double num) {
-		if (num<0) {
+		if (num<0) { //returns the absolute value of a number
 			return(num);
 		} else {
 			return(num);
@@ -66,7 +67,7 @@ public class Calculate {
 	}
 	
 	public static int max(int num1, int num2) {
-		if (num1>=num2) {
+		if (num1>=num2) { //returns the greater number
 			return num1;
 		} else {
 			return num2;
@@ -74,17 +75,11 @@ public class Calculate {
 	}
 		
 	public static double max2(int num1, int num2, int num3) {
-		if (num1>num2) {
+		if (num1>num2 && num1>num3) { //returns the greater of 3 numbers
 			return num1;
-		} else if (num1<num2) {
+		} else if (num2>num1 && num2>num3) {
 			return num2;
-		} else if (num1>num3) {
-			return num1;
-		} else if (num1<num3) {
-			return num3;
-		} else if (num2>num3) {
-			return num2;
-		} else {
+		} else if (num3>num1 && num3>num1) {
 			return num3;
 		}
 	}
@@ -125,7 +120,7 @@ public class Calculate {
 	}
 		
 	
-	public static boolean isPrime(int n) {
+	public static boolean isPrime(int n) { //determines whether a number is prime or not.
 		for(int i=2;i<n;i++) {
 	    	if(n%i==0) {
 	    		return false;
@@ -135,29 +130,29 @@ public class Calculate {
 		}
 	}
 
-	public int gcf(int a, int b) {
-	    if (b == 0){
-	        return Math.abs(a);
+	public int gcf(int num1, int num2) {
+	    if (num2 == 0){
+	        return Math.abs(num1);
 		}else{ 
-	        return Math.abs((gcf (b, a % b)));
+	        return Math.abs((gcf (num2, num1 % num2)));
 	    }
 	}
 		
 	
 	public static double sqrt(double num1) {
-		double t;
+		double x;
 	 
 		double squareRoot = num1 / 2;
 	 
 		do {
-			t = squareRoot;
-			squareRoot = (t + (num1 / t)) / 2;
-		} while ((t - squareRoot) != 0);
+			x = squareRoot;
+			squareRoot = (x + (num1 / x)) / 2;
+		} while ((x - squareRoot) != 0);
 	 
 		return squareRoot;
 	}
 	
-	public static 
+	public static  quadform
 
 		
 	
--- Collaborative Programming Exercise/src/ProcessingNumbers.java
+++ Collaborative Programming Exercise/src/ProcessingNumbers.java
@@ -1,15 +1,22 @@
+//Harry Law 10/5/2016
+
 import java.util.*;
 public class ProcessingNumbers {
 	
 	public static void main (String[]args){
-		Scanner firstInput = new Scanner(System.in);
-		System.out.println("Please enter your first value");
-		int val1 = firstInput.nextInt();
-		Scanner secondInput = new Scanner (System.in);
-		System.out.println("Please enter your second value");
-		int val2 = secondInput.nextInt();
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter your value(s)");
+		int val1 = userInput.nextInt();
+		int val2 = userInput.nextInt();
 		if(val1%2 == 0 && val2%2 == 0){
-			
+			System.out.println("Your numbers are good. ");
+			if (val1>val2){
+				System.out.println(Math.abs(val1));
+			}else{
+				System.out.println(Math.abs(val2));
+			}
+		}else{
+			System.out.println("You need even numbers. "); //Try entering different values.
 		}
 	}
 
