0722a381b686509176f39d133b1088bc23693377
harryLaw14 <17lawh76@millshighsch.org>
2016-11-02 11:27:43 -0700

    im so done

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -25,11 +25,11 @@ public class Calculate {
 	}
 	
 	public static double toDegrees(double number) { //converts to degrees
-		return((number*3.14159)*180); 
+		return((number*3.14159)/(3.14159/180)); 
 	}
 	
 	public static double toRadians(double number) { //converts to radians
-		return((number/3.14159)/180);
+		return((number*3.14159)/180);
 	}
 	
 	public static double discriminant(double a, double b, double c) {
@@ -74,15 +74,15 @@ public class Calculate {
 		}
 	}
 		
-	public static double max (int num1, int num2, int num3) {
+	public static double max (double num1, double num2, double num3) {
 		if (num1>num2 && num1>num3) { //returns the greater of 3 numbers
 			return num1;
 		} else if (num2>num1 && num2>num3) {
 			return num2;
 		} else if (num3>num1 && num3>num1) {
 			return num3;
-		}else{
-			return(0);
+		} else if (){
+			return 0;
 		}
 	}
 	
@@ -154,6 +154,23 @@ public class Calculate {
 	 
 		return squareRoot;
 	}
+	
+	public static String quadForm(int a, int b, int c){
+		double discriminant = Calculate.discriminant(a, b, c);
+		if (discriminant < 0){
+			return "There is no real roots";
+			
+		}else if (discriminant == 0){
+			double rootOne = (-b+discriminant)/(2*a);
+			return "The Root = " +rootOne;
+			
+		}else{
+			double rootone = (-b -discriminant)/(2*a);
+			rootone = Calculate.round2(rootone);
+		}
+	}
+	
+	
 	
 }
 
