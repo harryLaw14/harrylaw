310bbb541402d1af7a35b8f6ff7f4a59aa8e7e18
harryLaw14 <17lawh76@millshighsch.org>
2016-11-21 22:00:06 -0800

    splitstring almost complete

--- Srting Split/src/Split.java
+++ Srting Split/src/Split.java
@@ -3,8 +3,17 @@ public class Split
 {
 
 	public static void main(String[] args) {
-		
+		midBread("lettustomatobaconmayohambreadcheese");
+		midBread("applespineapplesbreadlettustomatobaconmayoham");
+		midBread("applespineapplesbreadlettustomatobaconmayohambreadcheese");
+		midBread("applespineapplesbreadlettustomatobaconmayohambreadcheesebreadcheese");
+		midBread("applespineapplesbreadlettustomatobaconmayohambreadcheesebread");
+		midBread("breadapplesbread");
+		midBread("breadbreadbread");
+		spaceMidBread("apples pineapples bread lettus tomato bacon mayo ham bread cheese");
 	}
+
+
 		//String.split();
 		//It's a method that acts on a string, <StringName>.split(<String sp>);
 		//Where sp is the string where the string splits
@@ -16,53 +25,51 @@ public class Split
 		
 		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
 		
-		public static String partone; {
-			String test = "I really like really apples";
-			String [] testing = test.split("really");
-			for (int i=0; i<testing.length;i++){
-				System.out.print(testing[i] + " ");
+		public static void spaceMidBread(String bread){
+			String[] firstbread = bread.split(" bread ");
+			if (firstbread.length>=1){
+				for (int i = 1; i < firstbread.length - 1; i++){
+					System.out.println(firstbread[i]);
+				}
 			}
+			else{
+				System.out.println("Not a real sandwich.");
 		}
+	}
 		//Your task:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
 		 * use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		 * What if it's a fancy sandwich with multiple pieces of bread?
 		*/
 		
-		public static String parttwo;{	
-			System.out.println();
-			String a="applespineapplesbreadlettustomatobaconmayohambreadcheese"; 
-			String[] bigsandwich =a.split("bread");
-			for (int j=0; j<bigsandwich.length; j++){
-				System.out.print(substring.);
+		public static void midBread(String bread){
+			bread = "apples" + bread + "apples";
+			String[] firstbread = bread.split("bread");
+			String[] secondbread = new String[firstbread.length];
+			if (firstbread.length >= 1){
+				for(int j = 0; j< firstbread.length; j++){
+					secondbread[j] = firstbread[j] + ", ";
+				}
+				String[] thirdbread = sandwich.toString(secondbread).split(", ");
+				for(int k = 1; k <= secondbread.length; k++){
+					System.out.print(thirdbread[k]);
+				}
+				System.out.println();
+			}
+			else{
+				System.out.println("Not a real sandwich.");
 			}
 		}
+
 		//Your task pt 2:
 		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
 		
-		public static void middleBread(String bread){
-			bread = "apples" + bread + "apples";
-			String[] bread1 = bread.split("bread");
-			String[] bread2 = new String[bread1.length];
-			if (bread1.length > 2){
-				for(int i = 0; i < bread1.length; i++){
-					bread2[i] = bread1[i] + ", ";
-				}
-				String[] bread3 = arrays.toString(bread2).split(", ");
-				for(int j = 1; j <= bread2.length; j++){
-					System.out.print(bread3[j]);
-				}
-				System.out.println();
-			}
-			else{
-				System.out.println("you don't have a sandwich");
-			}
-		}
+}
 
-}		
+			
 
 
 
