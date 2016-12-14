625f729222c438f6f9a3aedaeb3356d3407bb31b
harryLaw14 <17lawh76@millshighsch.org>
2016-12-14 10:44:09 -0800

    acslland

---
+++ ACSLLand/src/ACSLGAME.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ACSLGAME {
+	
+	public static void main (String[] args){
+		for (int i=1;i<=5;i++){
+			
+		}
+	}
+	
+	public static int getRolls (){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter inputs here plz. End the program by typing 0.");
+		
+		
+	}
+}
